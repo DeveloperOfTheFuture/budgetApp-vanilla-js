ef3f6a87a61be58696fa7e8def85bd4eb927f33f
Stolbov Ilya <kretov.il@mail.ru>
2020-06-18 19:40:37 +0300

    Функция обновления баланса, доходов и расходов готова

--- app.js
+++ app.js
@@ -6,11 +6,26 @@ const form = document.querySelector('#form');
 const text = document.querySelector('#text');
 const amount = document.querySelector('#amount');
 
-const dummyTransactions = [
-  {id: 1, text: 'App', amount: -20},
-  {id: 2, text: 'Salary', amount: 520},
-  {id: 3, text: 'Game', amount: -100},
-  {id: 4, text: 'Bank transfer', amount: 10},
+const dummyTransactions = [{
+    id: 1,
+    text: 'App',
+    amount: -20
+  },
+  {
+    id: 2,
+    text: 'Salary',
+    amount: 520
+  },
+  {
+    id: 3,
+    text: 'Game',
+    amount: -100
+  },
+  {
+    id: 4,
+    text: 'Bank transfer',
+    amount: 10
+  },
 ];
 
 let transactions = dummyTransactions;
@@ -28,11 +43,27 @@ function addTransactionDOM(transaction) {
   list.appendChild(item);
 }
 
+// Update balance, income, expense
+function updateValues() {
+  const amounts = transactions.map(transaction => transaction.amount);
+
+  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+
+  const income = amounts.filter(item => item > 0).reduce((acc, item) => (acc += item), 0).toFixed(2);
+
+  const expense = (amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) * -1).toFixed(2);
+
+  balance.textContent = `$${total}`;
+  moneyPlus.textContent = `$${income}`;
+  moneyMinus.textContent = `$${expense}`
+}
+
 // Init transactions
 function init() {
   list.innerHTML = '';
 
-  transactions.forEach(addTransactionDOM)
+  transactions.forEach(addTransactionDOM);
+  updateValues();
 }
 
 init();
